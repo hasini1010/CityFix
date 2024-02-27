229d2b0db736e3f0b1bbbd10db082a6f3e073c18
Hasini Kanchetty <139251129+hasini1010@users.noreply.github.com>
2024-02-27 23:26:34 +0530

    maps done

--- reportuser.js
+++ reportuser.js
@@ -1,7 +1,7 @@
 const dropArea=document.getElementById("drop-area");
 const inputFile=document.getElementById("input-file");
 const imageView=document.getElementById("img-view");
-
+const imageLinkParagraph = document.getElementById("image-link");
 inputFile.addEventListener("change",uploadImage);
 
 function uploadImage()
@@ -11,7 +11,7 @@ function uploadImage()
     imageView.style.backgroundImage=`url(${imgLink})`;
     imageView.textContent="";
     imageView.style.border=0;
-
+    imageLinkParagraph.textContent = "Image Link: " + imgLink;
 }
         function toggleCheckbox(selectedCard) {
             var cards = document.querySelectorAll('.card');
