7af6be9462da99c53cc4d20a868be153ad037686
Hasini Kanchetty <139251129+hasini1010@users.noreply.github.com>
2024-02-28 19:37:58 +0530

    done

--- reportuser.js
+++ reportuser.js
@@ -27,3 +27,30 @@ function uploadImage()
             var checkmark = selectedCard.querySelector('.checkmark');
             checkmark.style.display = selectedCard.classList.contains('selected') ? 'block' : 'none';
         }
+
+        const findMyState = ()=>{
+            const status=document.querySelector('.status');
+
+            const success =(position)=>
+            {
+                console.log(position);
+                const latitude=position.coords.latitude;
+                const  longitude=position.coords.longitude;
+                //console.log(latitude +' '+longitude);
+                const geoApiUrl=`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
+                fetch(geoApiUrl)
+                .then(res=>res.json())
+                .then(data=>{
+                    const place=data.locality;
+                })
+            }
+            const error =()=>
+            {
+                status.textContent ='Unable to retrieve your location';
+            }
+            navigator.geolocation.getCurrentPosition(success,error);
+        }
+        document.querySelector('.find-state').addEventListener('click',findMyState);
+        localStorage.setItem('LAT',latitude);
+        localStorage.setItem('LON',longitude);
+        localStorage.setItem('LOC',place);
